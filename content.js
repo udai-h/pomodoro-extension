3037cfd08be62e51144dcfe96b576980747fa22c
udai-h <yudai1.hinata@gmail.com>
2025-06-01 09:42:08 +1000

    min function with moving progress bar

--- content.js
+++ content.js
@@ -1,3 +1,13 @@
 const bar = document.createElement("div");
 bar.id = "pomodoro-bar";
 document.body.appendChild(bar);
+
+let width = 0;
+const interval = setInterval(() => {
+  width += 1;
+  bar.style.width = width + "%";
+
+  if (width >= 100) {
+    clearInterval(interval);
+  }
+}, 1000);
